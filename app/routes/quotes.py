92a5c1a769178c91e09b779a18d6837d3e962336
sbcybersolutions <christian@sbcyber.net>
2025-05-28 07:34:23 +0200

    updated quotes.html and added quote_detail.html

--- app/routes/quotes.py
+++ app/routes/quotes.py
@@ -6,3 +6,9 @@ from . import main
 def quote_list():
     quotes = Quote.query.order_by(Quote.created_at.desc()).all()
     return render_template('quotes.html', quotes=quotes)
+
+# Quote details route
+@main.route('/quote/<int:quote_id>')
+def quote_detail(quote_id):
+    quote = Quote.query.get_or_404(quote_id)
+    return render_template('quote_detail.html', quote=quote)
